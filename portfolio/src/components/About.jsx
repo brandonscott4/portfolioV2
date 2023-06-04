f50916c6261948e2bd78fcc1ca9e0ad603dbc2d4
brandonscott4 <bscott15@sky.com>
2023-06-04 17:10:01 +0100

    added degree summary modal

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -7,15 +7,21 @@ function About() {
           <p> I am a developer based in the UK (England) 📍.</p>
           <br />
           <p>
-            Currently in my final year of my Computer Science degree (see more),
-            and looking to break into the tech industry. Learning new
+            Currently in my final year of my Computer Science degree&nbsp;
+            <span
+              className="link link-primary"
+              onClick={() => window.my_modal_1.showModal()}
+            >
+              (see more)
+            </span>
+            , and looking to break into the tech industry. Learning new
             technologies and enhancing my knowledge of familair ones is a
             rewarding journey, that I am througly enjoying.
           </p>
           <br />
           <p>
             In my free time I enjoy watching/playing football, playing video
-            games on my PC (which I built myself!) and reading.
+            games on my PC (which I built myself) and reading.
           </p>
         </div>
 
@@ -88,6 +94,44 @@ function About() {
             </div>
           </div>
         </div>
+        <dialog id="my_modal_1" className="modal">
+          <form method="dialog" className="modal-box max-w-3xl">
+            <h3 className="font-bold text-lg mb-4">Degree Summary 📜</h3>
+
+            <div className="flex gap-10">
+              <div>
+                <h4 className="underline">First Year</h4>
+                <ul>
+                  <li>Introduction to Object-Oriented Programming - 98</li>
+                  <li>Further Object-Oriented Programming - 89</li>
+                  <li>Foundations of Computing I - 98</li>
+                  <li>Foundations of Computing II - 91</li>
+                  <li>Databases and the Web - 92</li>
+                  <li>Human Computer Interaction - 85</li>
+                  <li>Computers and the Cloud - 71</li>
+                  <li>Problem Solving with Algorithms -83</li>
+                </ul>
+              </div>
+
+              <div>
+                <h4 className="underline">Second Year</h4>
+                <ul>
+                  <li>Algorithms, Correctness and Efficiency - 96</li>
+                  <li>Introduction to Artificial Intelligence - 94</li>
+                  <li>Database Systems - 93</li>
+                  <li>Web Development - 100</li>
+                  <li>Functional Programming - 99</li>
+                  <li>Computer Systems - 92</li>
+                  <li>Introduction to Cyber Security - 89</li>
+                  <li>Software Development - 87</li>
+                </ul>
+              </div>
+            </div>
+            <div className="modal-action">
+              <button className="btn">Close</button>
+            </div>
+          </form>
+        </dialog>
       </div>
     </>
   );
